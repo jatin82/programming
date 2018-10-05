5256d9e40a671ee3c7e3d82494ba09ecac7ad2bf
Jatin <jatin.sokhal@pearson.com>
2018-10-05 18:10:01 +0530

    Init

--- MyDefination/Number.ts
+++
@@ -1,9 +0,0 @@
-interface Number{
-    compareTo(x:number):number;
-}
-
-Number.prototype.compareTo = function(x){
-    if(x === this) return 0;
-    else if(x>this) return -1;
-    else return 1;
-};
