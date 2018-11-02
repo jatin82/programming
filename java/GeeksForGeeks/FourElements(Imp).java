c46fbc68d0b51ee529be703340572257e9cab405
Jatin <jatin.sokhal@pearson.com>
2018-11-02 13:16:41 +0530

    Very important four elements

--- java/GeeksForGeeks/FourElements(Imp).java
+++ java/GeeksForGeeks/FourElements(Imp).java
@@ -1,13 +1,45 @@
 /*
 https://practice.geeksforgeeks.org/problems/four-elements/0
 
-*/
+2
+3
+2 2 1
+6
+6
+1 5 1 0 6 0
+7
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+output:
+0
+1
+
+*/
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
 
 class GFG {
+	
+	static class node{
+		int first;
+		int second;
+		int sum;
+		
+		public node(int first,int second,int sum) {
+			this.first = first;
+			this.second = second;
+			this.sum = sum;
+		}
+		public String toString() {
+			return first + "-" + second +" = "+sum;
+		}
+	}
+	
+	
 	public static void main (String[] args) {
 	
 	    Scanner sc = new Scanner(System.in);
@@ -17,28 +49,43 @@ class GFG {
 	        int n = sc.nextInt();
 
 	        int [] a = new int[n];
-	        int [] aux = new int[(n*(n-1)/2)];
+	        List<node> aux = new ArrayList();
+	        
 	        
 	        for(int i=0;i<n;i++){
+	            a[i] = sc.nextInt();
+	        }
+	        
+	        int x = sc.nextInt();
+	        
+	        int index = 0;
+
+	        for(int i=0;i<n;i++){
 	            for(int j=i+1;j<n;j++){
-	                int index = i * (n-1-i) + j;
-	                aux[index] = a[i] + a[j];
+					int temp = a[i] + a[j];
+					aux.add(new node(i,j,temp));
 	            }
 	        }
-	        int x = sc.nextInt();
-	        Arrays.sort(aux);
 	        
-	        Set<Integer> sum = new HashSet<>();
+	        //Collections.sort(aux, (node a1,node a2) -> a1.sum - a2.sum );
 	        
-	        for(int i=0;i<aux.length;i++){
-	            sum.add(x-aux[i]);
+	        Map<Integer,node> sum = new HashMap<>();
+	        
+			
+	        for(int i=0;i<aux.size();i++){
+	            sum.put(x-aux.get(i).sum,aux.get(i));
 	        }
 	        
 	        boolean isFour = false;
 	        
-	        for(int ele:sum){
-	            if(sum.contains(ele)){
-	                isFour = true;
+	        for(int i=0;i<aux.size() && !isFour;i++){
+
+	            if(sum.containsKey(aux.get(i).sum)){
+	                node current = aux.get(i);
+	                node got = sum.get(current.sum);
+	                
+	                if(current.first!=got.first && current.first!=got.second && current.second!=got.second && current.second!=got.first)
+	                    isFour = true;
 	            }
 	        }
 	        
