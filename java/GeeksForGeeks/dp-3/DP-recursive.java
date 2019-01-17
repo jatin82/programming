6ac2edf1750cce4416c1c90104d56021a177ca4c
Jatin Sokhal <jatin.sokhal3@gmail.com>
2019-01-17 19:09:17 +0530

    Added Lis mistake

--- java/GeeksForGeeks/dp-3/DP-recursive.java
+++ java/GeeksForGeeks/dp-3/DP-recursive.java
@@ -7,8 +7,14 @@ class Test2{
 8
 5 10 30 21 22 24 25 40
 
+
+16
+0 8 4 12 2 10 6 14 1 9 5 13 3 11 7 15
+
+6
+0 8 4 12 2 10
+
  */
-	
 	
 	private static int findLisRec(int [] a,int n) {
 
@@ -18,7 +24,7 @@ class Test2{
 			return 1;
 		
 		for(int i=0;i<n;i++) {
-			int res = findLis(a,i);
+			int res = findLisRec(a,i);
 			if(a[i] < a[n] && (res+1)>maxLis) {
 				maxLis = res + 1;
 			}
@@ -28,21 +34,22 @@ class Test2{
 	}
 	
 	private static int findLis(int [] a,int n) {
-		
 		int[] lis = new int[n]; 
-		lis[0] = 1;
-		
+		int maxLisSoFar=-1;
 		for(int j=0;j<n;j++) {
 			int maxLis = 1;
 			for(int i=0;i<j;i++) {
-				int res = lis[i];
-				if(a[i] < a[j] && (res+1)>maxLis) {
-					maxLis = res + 1;
+				int res = lis[i] + 1;
+				if(a[i] < a[j] && res>maxLis) {
+					maxLis = res;
 				}
 			}
 			lis[j] = maxLis; 
+			if(maxLisSoFar < lis[j]) {
+				maxLisSoFar = lis[j];
+			}
 		}
-		return lis[n-1];
+		return maxLisSoFar;
 	}
 	
 	
